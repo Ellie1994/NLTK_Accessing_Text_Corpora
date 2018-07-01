c143c39b5ff8e595804777838c6cdcc265531593
Elvira <40444030+Ellie1994@users.noreply.github.com>
2018-07-01 20:22:25 +0200

    Update 1.3.Gutenberg_long_short_sent.py

--- 1.3.Gutenberg_long_short_sent.py
+++ 1.3.Gutenberg_long_short_sent.py
@@ -3,11 +3,11 @@ from nltk.corpus import gutenberg
 hamlet_sents = gutenberg.sents('shakespeare-hamlet.txt')
 print(hamlet_sents) # prints out the introduction
 
-print() # prints out a blank string 
+print() # prints out a blank line
 
 print(hamlet_sents[19]) # finds one particular sentence by its number in a text
 
-print() # prints out a blank string 
+print() # prints out a blank line 
 
 longest_sent = max(len(s) for s in hamlet_sents) # prints out a number of the longest sentence
 
